0f41e21240e115873d8ded43778f15b11babfe3a
Gerza <topuzzino@hotmail.de>
2018-07-06 20:34:32 +0200

    в попытках найти ошибку в фильтрах

--- js/filters.js
+++ js/filters.js
@@ -3,26 +3,30 @@
 (function () {
   var formFilters = document.querySelector('.map__filters');
   // window.data.PIN_NUMBER;
+  var MIN_PRICE = 10000;
+  var MAX_PRICE = 50000;
 
 
-  var housingType = document.getElementById('housing-type');
-  var housingPrice = document.getElementById('housing-price');
-  var housingRooms = document.getElementById('housing-rooms');
-  var housingGuests = document.getElementById('housing-guests');
+  var housingType = document.querySelector('#housing-type');
+  var housingPrice = document.querySelector('#housing-price');
+  var housingRooms = document.querySelector('#housing-rooms');
+  var housingGuests = document.querySelector('#housing-guests');
   var housingFeatures = document.querySelectorAll('.map__checkbox');
+
 
   var filterType = function (objectItem) {
     return objectItem.offer.type === housingType.value || housingType.value === 'any';
   };
 
+
   var filterPrice = function (objectItem) {
     switch (housingPrice.value) {
       case 'low':
-        return objectItem.offer.price <= 10000;
+        return objectItem.offer.price <= MIN_PRICE;
       case 'middle':
-        return objectItem.offer.price > 10000 && objectItem.offer.price < 50000;
+        return objectItem.offer.price > MIN_PRICE && objectItem.offer.price < MAX_PRICE;
       case 'high':
-        return objectItem.offer.price >= 50000;
+        return objectItem.offer.price >= MAX_PRICE;
       default:
         return objectItem;
     }
@@ -48,13 +52,24 @@
 
   var filterAllAds = function (objectsList) {
     var copyObjectsList = objectsList.slice();
-    var filteredAds = copyObjectsList.filter(function (objectItem) {
-      return filterType(objectItem) && filterPrice(objectItem) && filterRooms(objectItem) && filterGuests(objectItem) && filterFeatures(objectItem);
-    }).slice(0, window.data.PIN_NUMBER);
-    return filteredAds;
+    var filteredAds = copyObjectsList.filter(function (copyObjectsList) {
+      return filterType(copyObjectsList) && filterPrice(copyObjectsList) && filterRooms(copyObjectsList) && filterGuests(copyObjectsList) && filterFeatures(copyObjectsList);
+    });
+    return filteredAds.clice(0, window.data.PIN_NUMBER);
   };
 
-  formFilters.addEventListener('change', window.debounce(filterAllAds));
+  console.log(filterAllAds(window.data.objectsList));
+
+  formFilters.addEventListener('change', window.debounce(filterAllAds(window.data.objectsList)));
+
+  /*
+  housingType.addEventListener('change', function () {
+      debugger
+      if (window.data.objectsList) {
+        filterType(window.data.objectsList);
+      }
+    });
+  */
 
   window.filters = {
     filterAllAds: filterAllAds
